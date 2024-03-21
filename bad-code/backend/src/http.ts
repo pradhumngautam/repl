3480fbb7437ad36c41c42b7058eb2947137f7ea0
Pradhumn Gautam <pradhumngautam0506@gmail.com>
2024-03-21 22:58:06 +0530

    prettier

--- bad-code/backend/src/http.ts
+++ bad-code/backend/src/http.ts
@@ -3,19 +3,19 @@ import { copyS3Folder } from "./aws";
 import express from "express";
 
 export function initHttp(app: Express) {
-    app.use(express.json());
+  app.use(express.json());
 
-    app.post("/project", async (req, res) => {
-        // Hit a database to ensure this slug isn't taken already
-        const { replId, language } = req.body;
+  app.post("/project", async (req, res) => {
+    // Hit a database to ensure this slug isn't taken already
+    const { replId, language } = req.body;
 
-        if (!replId) {
-            res.status(400).send("Bad request");
-            return;
-        }
+    if (!replId) {
+      res.status(400).send("Bad request");
+      return;
+    }
 
-        await copyS3Folder(`base/${language}`, `code/${replId}`);
+    await copyS3Folder(`base/${language}`, `code/${replId}`);
 
-        res.send("Project created");
-    });
+    res.send("Project created");
+  });
 }
